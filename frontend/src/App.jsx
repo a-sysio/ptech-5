b86140d29af87fe3e4fd7c611b33070cc34c3079
pituuu <sysio.olek@gmail.com>
2025-02-06 19:12:39 +0000

    Update komponentu App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{width: '400px', margin: 'auto'}}>
+      <LocalizationProvider dateAdapter={AdapterDatyjs}>
+        <Button onClick={() => setOpen(true)}>Dodaj zadanie</Button>
+        <CreateTodo categories={categories} open={open} onSave={handleSave}/>
+        <TodoList todos={todos} onDelete={handleDelete} onUpdate={handleUpdate} onCompletion={handleCompletion}/>
+      </LocalizationProvider>
+    </div>
   )
+}
+
+useEffect(() => {
+  const url = `${API_BASE}/categories`
+  axios.get(url).then(response => setCategories(response.data))
+}  , [])
+
+useEffect(() => {
+    getTodos()
+}, [])
+
+const [categories, setCategories] = useState([])
+const [todos, setTodos] = useState([])
+const [open, setOpen] = useState(false)
+
+const getTodos = () => {
+  const url = `${API_BASE}/todos`
+  axios.get(url).then(response => setTodos(response.data))
+}
+
+const handleSave = (title, description, category, dueDate, completed) => {
+  const url = `${API_BASE}/todos`
+  const data = {
+    title,
+    description,
+    category,
+    due_date: dueDate,
+    completed: completed == 'true' ? true : false,
+  }
+
+  axios.post(url, data).then(response => {
+    setOpen(false)
+    getTodos()
+  })
+}
+
+const handleDelete = (id) => {
+  const url = `${API_BASE}/todos/${id}`
+  axios.delete(url).then(response => {
+    getTodos()
+  })
+}
+
+const handleUpdate = (todo) => {
+}
+
+const handleCompletion = (todo) => {
 }
 
 export default App
